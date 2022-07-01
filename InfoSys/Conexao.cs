d9500ecba368316ac5d4925709aa5974f56de8c0
Adiel Cardoso <62920887+AdielCardosoDev@users.noreply.github.com>
2022-06-30 21:56:52 -0300

    Conexão

--- InfoSys/Conexao.cs
+++ InfoSys/Conexao.cs
@@ -2,10 +2,10 @@
 {
     internal class Conexao
     {
-        public static string server = @"ADIELCARDOSO\ESTUDOS";
-        public static string database = "DB_SysClient";
-        public static string user = "sa";
-        public static string password = "1234";
+        public static string server = @"";
+        public static string database = "";
+        public static string user = "";
+        public static string password = "";
 
 
 
